0f96fc1deb26a1fc3346bf1a0ee6f411eea57293
Niraj <niraj.webtech@gmail.com>
2019-03-21 04:48:36 +0530

    Removing @RestController Fron NEws Action  File

--- src/main/java/org/niraj/action/NewsAction.java
+++ src/main/java/org/niraj/action/NewsAction.java
@@ -2,17 +2,16 @@ package org.niraj.action;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @Controller
-@RequestMapping(value="/news")
+@RequestMapping("/news")
 public class NewsAction {
 
 	
 	@RequestMapping(value="/daily")
 	public String dailyNews(){
 		System.out.println("This is daily News Module Welcome page");
-		return "Your Daily News";
+		return "dailyNews";
 	}
 	
 }
